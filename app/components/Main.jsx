5e88e98f628f06b1ae5e96739fceb7d9b4118c86
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2018-02-27 15:32:49 -0500

    added potentially styling

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,10 +4,10 @@ import TodoApp from "TodoApp";
 var Main = (props) => {
 	return(
 			<div>
-        <h3 className='page-title'>Scholastic Quotation Explorer</h3>
-
+        <h3 className='page-title'><em>Ad fontes</em>: A Scholastic Quotation Explorer</h3>
 				<div className='page-header'>
-					<p>A <a href="http://lombardpress.org">LombardPress</a> Publication | Powerd by Data from the <a href="https://scta.info">Scholastic Commentaries and Text Archive</a></p>
+					<p>A <a href="http://lombardpress.org">LombardPress</a> Publication</p>
+					<p>Powerd by Data from the <a href="https://scta.info">Scholastic Commentaries and Text Archive</a></p>
 					<p>Designed by <a href="http://jeffreycwitt.com">Jeffrey C. Witt</a></p>
         </div>
         <TodoApp/>
