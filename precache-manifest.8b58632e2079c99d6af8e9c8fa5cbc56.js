0fb95a388acee01dac5449fb84008a1c6a9a982a
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2019-08-15 09:48:52 -0400

    Updates

---
+++ precache-manifest.8b58632e2079c99d6af8e9c8fa5cbc56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf8823baf2c34fa8122564315016842",
+    "url": "/adfontes/index.html"
+  },
+  {
+    "revision": "7baa02cd1b2e3b366357",
+    "url": "/adfontes/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9883001b0fcf1640011b",
+    "url": "/adfontes/static/css/main.81ae4fb5.chunk.css"
+  },
+  {
+    "revision": "7baa02cd1b2e3b366357",
+    "url": "/adfontes/static/js/2.f6790025.chunk.js"
+  },
+  {
+    "revision": "9883001b0fcf1640011b",
+    "url": "/adfontes/static/js/main.732c67f3.chunk.js"
+  },
+  {
+    "revision": "80b1d8c634162bfaefb0",
+    "url": "/adfontes/static/js/runtime~main.eb45c678.js"
+  }
+]);
