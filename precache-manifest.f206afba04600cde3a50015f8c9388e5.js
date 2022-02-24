a829f52c78e2ff2a5029e36a37af1f7778f2efeb
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2022-02-24 14:25:17 -0500

    Updates

---
+++ precache-manifest.f206afba04600cde3a50015f8c9388e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21085040a9bce46eff0ab45da9cf1325",
+    "url": "/adfontes/index.html"
+  },
+  {
+    "revision": "49fcb97c2385bc809c0d",
+    "url": "/adfontes/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "36f7b9d59cbc19ac61a6",
+    "url": "/adfontes/static/css/main.1f17c6fc.chunk.css"
+  },
+  {
+    "revision": "49fcb97c2385bc809c0d",
+    "url": "/adfontes/static/js/2.cb4ba28a.chunk.js"
+  },
+  {
+    "revision": "36f7b9d59cbc19ac61a6",
+    "url": "/adfontes/static/js/main.314805be.chunk.js"
+  },
+  {
+    "revision": "80b1d8c634162bfaefb0",
+    "url": "/adfontes/static/js/runtime~main.eb45c678.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/adfontes/precache-manifest.329a0466546c637672435ef9e1870a6d.js"
+  "/adfontes/precache-manifest.f206afba04600cde3a50015f8c9388e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adfontes/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
